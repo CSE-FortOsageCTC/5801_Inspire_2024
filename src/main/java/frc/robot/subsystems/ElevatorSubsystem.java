b626498b94ce8aa23b8a3f60d84cc7dc94cc6ba9
T-Wezzy <matthewrcb2007@gmail.com>
2024-02-23 16:27:12 -0600

    moved elevator stuff from shooter subsystem to elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import com.revrobotics.SparkLimitSwitch.Type;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,6 +11,7 @@ import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase{
     private static ElevatorSubsystem elevatorSubsystem;
+    private CANSparkMax shooterElevator;
 
     public SkyLimelight s_Limelight = SkyLimelight.getInstance();
     public Swerve s_Swerve = Swerve.getInstance();
@@ -25,6 +27,11 @@ public class ElevatorSubsystem extends SubsystemBase{
 
     public ElevatorSubsystem(){
         elevator = new CANSparkMax(25, MotorType.kBrushless);
+        shooterElevator = new CANSparkMax(25, MotorType.kBrushless);
+        
+        shooterElevator.getForwardLimitSwitch(Type.kNormallyClosed);
+        shooterElevator.getReverseLimitSwitch(Type.kNormallyClosed);
+        shooterElevator.burnFlash();
     }
 
     public double getElevatorValue(){
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.SparkLimitSwitch.Type;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -29,7 +28,6 @@ public class ShooterSubsystem extends SubsystemBase {
     private CANSparkMax topShooter;
     private CANSparkMax bottomShooter;
     private VictorSPX kicker;
-    private PIDController elevatorPID;
 
     private DoubleSolenoid shooterSolenoid;
 
