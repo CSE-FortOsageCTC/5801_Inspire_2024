7a020bf552c068fd80a7ec5b5159c5edddadcf89
Crwspy <jbanalesf2995@student.fortosage.net>
2024-03-02 21:29:44 -0600

    DONT SAY ANYTHING WE HAVE A PLAN but 2 piece (plus 2)

--- src/main/java/frc/robot/commands/AutoFlywheelCommand.java
+++ src/main/java/frc/robot/commands/AutoFlywheelCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.AlignPosition;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoFlywheelCommand extends Command{
@@ -10,23 +11,26 @@ public class AutoFlywheelCommand extends Command{
     public AutoFlywheelCommand(){
         shooterSubsystem = ShooterSubsystem.getInstance();
         count = 0;
+        AlignPosition.setPosition(AlignPosition.SpeakerPos);
         addRequirements(shooterSubsystem);
+
     }
     @Override
     public void execute(){
         shooterSubsystem.setFlyWheels(-1);
         count += 1;
-        if (count > 50){
+        if (count > 60){
             new AutoShootCommand().schedule();
         }
     }
     @Override
     public boolean isFinished(){
-        return count > 110;
+        return count > 70;
     }
     @Override
     public void end(boolean end){
         System.out.println("It ended");
+        //AlignPosition.setPosition(AlignPosition.Manual);
         count = 0;
     }
 }
